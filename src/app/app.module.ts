bef3ef8fb014e1c383bdd9628cd83eed6ebfce5b
unknown <samarthagarwal@live.com>
2017-12-12 17:08:23 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { File } from '@ionic-native/file'; 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
+let config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
@@ -23,7 +37,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen, FileChooser, File,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
+import { File } from '@ionic-native/file'
+import firebase from 'firebase'
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private fileChooser: FileChooser, private file: File) {
+
+  }
+
+  choose(){
+    this.fileChooser.open().then((uri)=>{
+      alert(uri);
+
+      this.file.resolveLocalFilesystemUrl(uri).then((newUrl)=>{
+        alert(JSON.stringify(newUrl));
+
+        let dirPath = newUrl.nativeURL;
+        let dirPathSegments = dirPath.split('/')            //break the string into an array
+        dirPathSegments.pop()                     //remove its last element
+        dirPath = dirPathSegments.join('/')
+
+        this.file.readAsArrayBuffer(dirPath, newUrl.name).then(async (buffer)=>{
+          await this.upload(buffer, newUrl.name);
+        })
+
+      })
+
+    })
+  }
+
+  async upload(buffer, name){
+    let blob = new Blob([buffer], { type: "image/jpeg"});
+
+    let storage = firebase.storage();
+
+    storage.ref('images/' + name).put(blob).then((d)=>{
+      alert("Done");
+    }).catch((error)=>{
+      alert(JSON.stringify(error))
+    })
 
   }
 
